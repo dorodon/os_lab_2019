16876dcc94c87199f2947cd73e8565619740379f
dorodon <alexandr.haiat@gmail.com>
2020-05-13 21:46:02 +0000

    z2 almost done

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -15,6 +15,8 @@
 #include "find_min_max.h"
 #include "utils.h"
 
+#include <assert.h>
+
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
@@ -22,15 +24,19 @@ int main(int argc, char **argv) {
   bool with_files = false;
 
   while (true) {
-    int current_optind = optind ? optind : 1;
+    int current_optind = optind ? optind : 1; //не используется
 
+    //массив структур: название, необходимость значения для данной опций, указатель на флаг, значение во флаг
+    //используется для обработки длинных опций
     static struct option options[] = {{"seed", required_argument, 0, 0},
                                       {"array_size", required_argument, 0, 0},
                                       {"pnum", required_argument, 0, 0},
                                       {"by_files", no_argument, 0, 'f'},
                                       {0, 0, 0, 0}};
 
-    int option_index = 0;
+    int option_index = 0; //индекс массива для перебора длинных опций
+    //"f" - есть только одна короткая опция без значения
+    //последовательно возращает код символа опции, если больше не будет найдено опций, которые надо распознать, вернет -1
     int c = getopt_long(argc, argv, "f", options, &option_index);
 
     if (c == -1) break;
@@ -39,32 +45,45 @@ int main(int argc, char **argv) {
       case 0:
         switch (option_index) {
           case 0:
+            //дробная часть числа отбросится
+            //если будет введено не число, вернет 0
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            printf("used seed = %d\n", seed);
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if (array_size < 1)
+            {
+                fprintf(stderr, "array_size = %d\n", array_size);
+            }
+            else 
+            {
+                printf("used array_size = %d\n", array_size);
+            }
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if (pnum < 1)
+            {
+                fprintf(stderr, "pnum = %d\n", pnum);
+            }
+            else 
+            {
+                printf("pnum = %d\n", pnum);
+            }
             break;
           case 3:
             with_files = true;
             break;
 
-          defalut:
+          default:
             printf("Index %d is out of options\n", option_index);
         }
         break;
       case 'f':
         with_files = true;
         break;
-
+      //символ не из строки с короткими опциями
       case '?':
         break;
 
@@ -77,7 +96,7 @@ int main(int argc, char **argv) {
     printf("Has at least one no option argument\n");
     return 1;
   }
-
+  //какая-либо из длинных опций не была введена
   if (seed == -1 || array_size == -1 || pnum == -1) {
     printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
            argv[0]);
@@ -86,6 +105,7 @@ int main(int argc, char **argv) {
 
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
+  PrintArray(array, array_size);
   int active_child_processes = 0;
 
   struct timeval start_time;
@@ -94,29 +114,42 @@ int main(int argc, char **argv) {
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
     if (child_pid >= 0) {
-      // successful fork
+      printf("fork() succeeded");
       active_child_processes += 1;
       if (child_pid == 0) {
-        // child process
-
-        // parallel somehow
+        // parallel somehow      
+        struct MinMax mM = GetMinMax(
+          array,
+          array_size * (active_child_processes - 1) / pnum,
+          pnum == active_child_processes? array_size: array_size * active_child_processes / pnum
+          );
 
         if (with_files) {
-          // use files here
+            // use files here
+            FILE *f;
+            f = fopen("buffer", "wb");
+            fwrite_unlocked(&mM, sizeof(struct MinMax), 1, f);   
+            fclose(f);       
         } else {
-          // use pipe here
+            // use pipe here
+            int fd[1];
+            pipe(fd);
+            write(fd[0], &mM, sizeof(struct MinMax));    
         }
         return 0;
       }
 
     } else {
-      printf("Fork failed!\n");
+      printf("Fork failed\n");
       return 1;
     }
   }
 
   while (active_child_processes > 0) {
     // your code here
+    int rt;
+    wait(&rt);
+    printf("status: %d\n", rt);
 
     active_child_processes -= 1;
   }
@@ -129,10 +162,18 @@ int main(int argc, char **argv) {
     int min = INT_MAX;
     int max = INT_MIN;
 
+    struct MinMax* data = malloc(sizeof(struct MinMax));
+
     if (with_files) {
       // read from files
+      FILE *f;
+      f = fopen("buffer", "rb");
+      fread(data, sizeof(struct MinMax), 1, f);
     } else {
       // read from pipes
+      int fd[1];
+      pipe(fd);
+      read(fd[0], data, sizeof(struct MinMax));
     }
 
     if (min < min_max.min) min_max.min = min;
--- lab3/src/z.sh
+++ lab3/src/z.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#z1
 cd /projects/dorodon-os_lab_2019/lab3/src/
 gcc -std=c99 find_min_max.c sequential_min_max.c utils.c
-./a.out 1 4
+./a.out 2 6
+#z2
+cd /projects/dorodon-os_lab_2019/lab3/src/
+gcc -std=c99 find_min_max.c parallel_min_max.c utils.c -o b.out
+./b.out --seed 1 --array_size 6 --pnum 2
